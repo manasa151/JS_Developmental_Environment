c50bc50dde6e72203913d21c6317e2cd29fdfa35
manasa151 <cornerstoneconnections@gmail.com>
2018-07-30 10:56:38 -0400

    all files are clean
    Signed-off-by: manasa151 <cornerstoneconnections@gmail.com>

---
+++ src/index.test.js
@@ -0,0 +1,21 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('index.html', () => {
+  it('should have h2 that says I have the will of fire', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function(err, window) {
+      const h2 = window.document.getElementsByTagName('h2')[0];
+      expect(h2.innerHTML).to.equal("I have the will of fire");
+      done();
+      window.close();
+    });
+  })
+})
